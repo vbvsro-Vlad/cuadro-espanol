b3718025246c47ae614b649d84ad725898bb247b
vbvsro-Vlad <vbvsro@gmail.com>
2026-08-01 09:49:49 +0300

    исправление ошибки текста
    В испанском тексте буква Y  написана была кирилицей. Сделано исправление на Y

--- data1.js
+++ data1.js
@@ -6,7 +6,7 @@ const database = [
     { start: 17.5, end: 23.2, es: "Sin pensar En piloto automatico Pero que dicen muchisimo sobre quienes somos", ru: "Не задумываясь. На автопилоте. Но которые очень многое говорят о том, кто мы такие." },
     { start: 23.2, end: 28.1, es: "Si estas aprendiendo espanol este episodio es perfecto para ti", ru: "Если вы учите испанский, этот эпизод идеален для вас" },
     { start: 28.1, end: 35.3, es: "Vocabulario del día a día, verbos reflexivos, expresiones naturales para hablar de tu rutina en español", ru: "Повседневная лексика, возвратные глаголы, естественные выражения для описания вашей повседневной жизни на испанском языке" },
-    { start: 35.3, end: 44, es: "Sin listas aburridas solo conversacion real entre dos personas reales Asi que escucha relajate и vamos alla", ru: "Без скучных списков, только реальный разговор между двумя реальными людьми. Так что слушай, расслабься и погнали." },
+    { start: 35.3, end: 44, es: "Sin listas aburridas solo conversacion real entre dos personas reales Asi que escucha relajate y vamos alla", ru: "Без скучных списков, только реальный разговор между двумя реальными людьми. Так что слушай, расслабься и погнали." },
     { start: 44, end: 45.1, es: "Vamos alla", ru: "Погнали." },
     { start: 45.1, end: 50, es: "Oye Lucia antes de empezar quiero preguntarte algo muy concreto", ru: "Послушай, Люсия, перед тем как начать, я хочу спросить тебя об одной очень конкретной вещи." },
     { start: 50, end: 50.6, es: "Dime", ru: "Скажи мне." },
